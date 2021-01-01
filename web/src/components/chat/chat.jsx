b19e3fd3f05f284129ccd5bad507cf4736d1b60c
jmacd229 <jmacd229@uwo.ca>
2021-01-01 16:44:27 -0500

    feat(chat): added facebook chat integration for production
    Added the chat component and attached it inside of the layout component

---
+++ web/src/components/chat/chat.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+
+export function Chat() {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: 'v9.0',
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, 'script', 'facebook-jssdk');
+  });
+
+  return (
+    <div
+      className='fb-customerchat'
+      attribution='install_email'
+      page_id='662192363886012'
+      theme_color='#55b8bb'
+      logged_in_greeting='Hi! How can I help you?'
+      logged_out_greeting='Hi! How can I help you?'></div>
+  );
+}
